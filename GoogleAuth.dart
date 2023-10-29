2e77769cccb53b0ab580f38e5c0c9b25a4956e75
Abhishvek Chavhan <52340362+abhishh1@users.noreply.github.com>
2023-10-29 21:34:37 +0530

    Update GoogleAuth.dart

--- GoogleAuth.dart
+++ GoogleAuth.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mango/Homepage.dart';
 
-
-// updated 
-
 String name, email, photoUrl;
 
 class GoogleAuthentication extends StatefulWidget {
